994dbf831bd10b5ff7862cd54f9c21a1cbaa9910
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-04-24 14:14:19 +0100

    some stuff....

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 'use strict';
 // JavaScript containing shared constants, for use in all pages.
 
-var backendIp = 'backend IP goes here';
-var token = 'your token goes here';
+var backendIp = 'ec2-99-81-194-20.eu-west-1.compute.amazonaws.com';
+var token = '0aa87f9e-009e-4ac5-a57f-f9711c4e5345';
 
 function buildUrl(path) {
     return 'http://' + backendIp + '/images' + path + '?token=' + token;
